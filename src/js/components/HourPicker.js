cee8c6c667c0faf69f085f64dc9240f508fbf738
daniel lukasik <daniel.lukasik10@gmail.com>
2020-01-14 22:22:24 +0100

    added date and hour functionality

---
+++ src/js/components/HourPicker.js
@@ -0,0 +1,48 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable no-undef */
+
+
+import BaseWidget from './BaseWidget.js';
+import { settings, select } from '../settings.js';
+import { utils } from '../utils.js';
+
+class HourPicker extends BaseWidget {
+    constructor(wrapper) {
+        super(wrapper, settings.hours.open);
+
+        const thisWidget = this;
+
+        thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+        thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+        thisWidget.value = thisWidget.dom.input.value;
+
+        thisWidget.initPlugin();
+
+    }
+    initPlugin() {
+        const thisWidget = this;
+
+        rangeSlider.create(thisWidget.dom.input);
+
+        thisWidget.dom.input.addEventListener('input', function () {
+            thisWidget.value = thisWidget.dom.input.value;
+        });
+    }
+    parseValue(value) {
+        const thisWidget = this;
+
+        thisWidget.hour = utils.numberToHour(value);
+
+        return thisWidget.hour;
+    }
+    isValid() {
+        return true;
+    }
+    renderValue() {
+        const thisWidget = this;
+
+        thisWidget.dom.output.innerHTML = thisWidget.value;
+    }
+}
+export default HourPicker;
